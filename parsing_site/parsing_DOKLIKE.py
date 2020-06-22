a9909e45e63892d1e2b1387482eb0a556065c8f7
gora5624 <gora5624@gmail.com>
2020-06-22 16:24:43 +0500

    Squashed commit of the following:
    commit 213146750be047bbb459e7fad8ed460e52d0c839
    Author: gora5624 <gora5624@gmail.com>
    Date:   Thu Jun 18 16:31:55 2020 +0500
    
         1

--- parsing_site/parsing_DOKLIKE.py
+++ parsing_site/parsing_DOKLIKE.py
@@ -4,6 +4,13 @@ import os
 import csv
 
 
+def write_csv(data, name_file='new_csv.csv', separator=';'):
+    with open(name_file, 'a', encoding='utf-8', newline='') as f:
+        writer = csv.DictWriter(
+            f, fieldnames=data.keys(), delimiter=separator)
+        writer.writerow(data)
+
+
 def get_html(url):
     '''Возвращает HTML код страницы по адресу url'''
 
@@ -37,57 +44,50 @@ def get_list_item(html, url_main):
 
     # Получаем список категорий которые есть на сайте
     list_main_category = get_list_main_category(html, url_main)
+    list_dict_item = []
     for main_category in list_main_category:  # Проходим по всем категориям
-         html = get_html(main_category['url_category'])
-          soup = BeautifulSoup(html, 'lxml')
-           try:  # В некоторых категориях несколько страниц, в некоторых только одна. Если страница одна, то блога pagination нет, отлавливаем эту ошибку, чтобы знать сколько парсить страниц
-                pages = int(
-                    len(soup.find('div', class_='pagination__pages').findChildren('a')))  # Пытаемся найти блок pagination
-            except AttributeError:  # Если его нет, то отлавливаем исключение, щадаём количество страниц и идём дальше
-                pages = 0
-            list_dict_item = []  # Создаём пустой список для заполнения товарами с сайта
-            if pages > 0:  # Если страниц несколько, то идём сюда
-                for page in range(pages+1):
-                    html_page = get_html(
-                        main_category['url_category'] + '?page={}'.format(page + 1))  # Получаем HTML код страницы, с учётом количества страниц подставляя нужную цифру методом format на каджой итерации цинкла for
-                    soup_page = BeautifulSoup(html_page, 'lxml')
-                    list_item_on_page = soup_page.find_all(
-                        'div', class_='product-item__link')  # Находит все товары на текущей странице и составляем список
-                    for item in list_item_on_page:  # Разворачиваем список и проходимся по всем элементам
-                        # Получем текст из блока с товаром - это название товара
-                        name_item = item.find('a').text
-                        url_item = item.find('a').get(
-                            'href')  # Получаем url товара
-                        dict_item = {'name_category': name_category,
-                                     'name_item': name_item,
-                                     'url_item': url_item}  # Создаем словарь с названием товара, его url и названием его категории
-                        # Добаляем словари в список
-                        list_dict_item.append(dict_item)
-            else:  # Если только одна страница - идём сюда, и всё аналогично
+        html = get_html(main_category['url_category'])
+        soup = BeautifulSoup(html, 'lxml')
+        try:  # В некоторых категориях несколько страниц, в некоторых только одна. Если страница одна, то блога pagination нет, отлавливаем эту ошибку, чтобы знать сколько парсить страниц
+            pages = int(
+                len(soup.find('div', class_='pagination__pages').findChildren('a')))  # Пытаемся найти блок pagination
+        except AttributeError:  # Если его нет, то отлавливаем исключение, щадаём количество страниц и идём дальше
+            pages = 0
+          # Создаём пустой список для заполнения товарами с сайта
+        if pages > 0:  # Если страниц несколько, то идём сюда
+            for page in range(pages+1):
                 html_page = get_html(
-                    main_category['url_category'])
+                    main_category['url_category'] + '?page={}'.format(page + 1))  # Получаем HTML код страницы, с учётом количества страниц подставляя нужную цифру методом format на каджой итерации цинкла for
                 soup_page = BeautifulSoup(html_page, 'lxml')
                 list_item_on_page = soup_page.find_all(
-                    'div', class_='product-item__link')
-                for item in list_item_on_page:
+                    'div', class_='product-item__link')  # Находит все товары на текущей странице и составляем список
+                for item in list_item_on_page:  # Разворачиваем список и проходимся по всем элементам
+                    # Получем текст из блока с товаром - это название товара
                     name_item = item.find('a').text
-                    url_item = item.find('a').get('href')
+                    url_item = item.find('a').get(
+                        'href')  # Получаем url товара
                     dict_item = {'name_item': name_item,
-                                 'url_item': url_item}
+                                 'url_item': url_item}  # Создаем словарь с названием товара, его url и названием его категории
+                    # Добаляем словари в список
                     list_dict_item.append(dict_item)
+        else:  # Если только одна страница - идём сюда, и всё аналогично
+            html_page = get_html(
+                main_category['url_category'])
+            soup_page = BeautifulSoup(html_page, 'lxml')
+            list_item_on_page = soup_page.find_all(
+                'div', class_='product-item__link')
+            for item in list_item_on_page:
+                name_item = item.find('a').text
+                url_item = item.find('a').get('href')
+                dict_item = {'name_item': name_item,
+                             'url_item': url_item}
+                list_dict_item.append(dict_item)
+    for item in list_dict_item:
+        write_csv(item)
 
-
-
-def get_information(html, url_main):
-
-  '''Получает информацию о товарах из списка, который передаёт функция get_list_item() и записывает его на диск'''
-
-  list_item = get_list_item(html, url_main)
-  for item in list_item:
-    
 
 url_main = 'https://odetdoctora.ru'  # Указывем адрес самого сайта
 # Указываем адрес страницы входа относительно адреса сайта
 url_in = url_main + '/products'
 html = get_html(url_in)  # Получаем HTML код страницы входа и начинаем работу
-get_information(html, url_main)
+get_list_item(html, url_main)
