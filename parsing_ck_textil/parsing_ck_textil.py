bf0d3cd4b089af3e18128570eeabff1882227f4b
gora5624 <gora5624@gmail.com>
2020-06-23 19:12:50 +0500

    Create parsing_ck_textil.py

---
+++ parsing_ck_textil/parsing_ck_textil.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import requests
+from bs4 import BeautifulSoup
+from my_lib import write_csv, read_csv, get_html
+import os
+import time
+
+
+get_information(url)
