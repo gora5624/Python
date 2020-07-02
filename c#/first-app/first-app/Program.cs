42d6d7d7b37e69d63244e5ae8352bce8dfbea594
gora5624 <gora5624@gmail.com>
2020-07-02 18:39:25 +0500

    Start c#

---
+++ c#/first-app/first-app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace first_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            string x  = Console.ReadLine();
+            int a = Convert.ToInt32(x);
+            a = a * a;
+            Console.WriteLine(a);
+
+        }
+    }
+}
