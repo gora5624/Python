5dcd3e30af73814ff43ff0febd839d53c05c8eff
gora5624 <gora5624@gmail.com>
2020-07-06 18:52:35 +0500

    debug

---
+++ c#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            summ summ = new summ();
+            Console.WriteLine(summ.GetSumm(10, 15));
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
