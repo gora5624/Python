6b95d0053941c0827158f9e81f51b02d83ae0e90
gora5624 <gora5624@gmail.com>
2021-09-06 15:31:11 +0500

    123

--- python/Make_print_WB/MakePrintForWB.py
+++ python/Make_print_WB/MakePrintForWB.py
@@ -11,23 +11,23 @@ pathToDonePrints = r'D:\printsPy'
 def getSizeAndPos(pathToMask):
     image = Image.open(pathToMask).convert("RGBA")
     size = image.size
-    for xLeft in range(100, size[0]):
+    for xLeft in range(0, size[0]):
 
         rgba = image.getpixel((xLeft, 1700))
         if rgba[3] != 255:
             break
         xLeft += 1
-    for xRight in reversed(range(size[0]-100)):
+    for xRight in reversed(range(size[0])):
         rgba = image.getpixel((xRight, 1700))
         if rgba[3] != 255:
             break
         xRight += 1
-    for yTop in range(100, size[1]):
+    for yTop in range(0, size[1]):
         rgba = image.getpixel((1900, yTop))
         if rgba[3] != 255:
             break
         yTop += 1
-    for yBott in reversed(range(size[1]-100)):
+    for yBott in reversed(range(size[1])):
         rgba = image.getpixel((1900, yBott))
         if rgba[3] != 255:
             break
@@ -47,8 +47,8 @@ def makePrint():
         maskImageOld = Image.open(pathToMask).convert("RGBA")
         BackgroundImageOld = Image.open(pathToBackground)
         xLeft, xRight, yTop, yBott, size = getSizeAndPos(pathToMask)
-        printsize = (xRight-xLeft + 10, yBott-yTop+10)
-        printPaste = (xLeft-5, yTop-5)
+        printsize = (xRight-xLeft, yBott-yTop)
+        printPaste = (xLeft, yTop)
         if not file_exists(os.path.join(pathToDonePrints, maskFolder)):
             os.makedirs(os.path.join(pathToDonePrints, maskFolder))
         for printPath in printList:
