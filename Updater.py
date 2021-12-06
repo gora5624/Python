fa6f23c6a3c3fc523ce449f9c6f795855728541d
gora5624 <gora5624@gmail.com>
2021-12-06 11:13:47 +0500

    123

--- Updater.py
+++
@@ -1,9 +0,0 @@
-import os
-import winshell
-import shutil
-
-
-path = r'{}'.format(
-    os.path.expanduser('~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup'))
-target = os.path.abspath(__file__)
-os.symlink(target, r'D:\tmp')
