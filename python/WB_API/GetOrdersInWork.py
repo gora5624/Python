2464c882a795c7f412aea49858788479e51b24e5
gora5624 <gora5624@gmail.com>
2021-12-28 14:33:31 +0500

    123

--- python/WB_API/GetOrdersInWork.py
+++ python/WB_API/GetOrdersInWork.py
@@ -487,7 +487,7 @@ def choiseMode():
     return mode
 
 
-def get_orders(Token, days=10):
+def get_orders(Token, days=30):
     """Получает заказы за последние 3 дня"""
     print("Идёт получение свежих заказов, ожидайте...")
     Url = 'https://suppliers-api.wildberries.ru/api/v2/orders?date_start={}%2B03%3A00&take=1000&skip={}'
