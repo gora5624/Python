60a6cb029d3d742a642fd21429460334f8231145
Georgyi <gora5624@gmail.com>
2022-03-24 14:46:46 +0500

    123

--- WB/WBUploadPhoto/Main.py
+++ WB/WBUploadPhoto/Main.py
@@ -4,14 +4,17 @@ import requests
 
 
 url = 'https://suppliers-api.wildberries.ru/upload/file/multipart'
-token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6ImEyNjQwNTAzLTk0NjktNGFkYy04MzVhLWM5MTQzZWU0NDBkYiJ9.UCh5I_5bnet0S2JcV92oDWS3p8RgUP5dsOwglCYu6ZE'
+token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjEyODkyYmRkLTEwMTgtNDJhNi1hYzExLTExODExYjVhYjg4MiJ9.nJ82nhs9BY4YehzZcO5ynxB0QKI-XmHj16MBQlc2X3w'
 fileID = str(uuid4())
-data = {'Content-Disposition': 'form-data'}
+data = {'Content-Disposition': 'form-data; name=uploadfile; filename=photo.jpg'}
 headers = {'Authorization': '{}'.format(token), 
+           'X-File-Id':fileID,
            'Content-Type': 'multipart/form-data;boundary=img',
-           'X-File-Id':fileID}
-files={'uploadfile': ('photo.jpg', open(r'F:\Done\69025844\photo\1.jpg', 'rb'), 'type=image/jpeg')}
-r = requests.post(url, headers=headers, files=files, data=data)
+           'Content-Disposition': 'form-data; name=uploadfile; filename=photo.jpg'}
+with open(r'F:\Done\69052696\photo\1.jpg', 'rb') as file:
+    a = file.read()
+files={'uploadfile': ('photo.jpg', a, 'type=image/jpeg')}
+r = requests.post(url, headers=headers, files=files)
 print(r.headers)
 pass
 
