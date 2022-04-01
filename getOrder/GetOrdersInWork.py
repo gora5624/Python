47d9fa0a4777f4b80bca9d65dbfe36160d8ccd24
Georgyi <gora5624@gmail.com>
2022-04-01 10:48:02 +0500

    123

--- getOrder/GetOrdersInWork.py
+++ getOrder/GetOrdersInWork.py
@@ -623,10 +623,10 @@ def choiseMode():
     return mode
 
 
-def get_orders(Token, days=7):
+def get_orders(Token, days=3):
     """Получает заказы за последние 3 дня"""
     print("Идёт получение свежих заказов, ожидайте...")
-    Url = 'https://suppliers-api.wildberries.ru/api/v2/orders?date_start={}%2B03%3A00&take=100&skip={}'
+    Url = 'https://suppliers-api.wildberries.ru/api/v2/orders?date_start={}%2B03%3A00&take=1000&skip={}'
 
     start_data = (datetime.today() - timedelta(days=int(days))).isoformat('T', 'seconds').replace(
         ':', '%3A').replace('+', '%2B').replace('.', '%2E')
@@ -650,7 +650,7 @@ def get_orders(Token, days=7):
                     continue
             except:
                 continue
-        count_skip = count_skip+100
+        count_skip = count_skip+1000
         tmp = response.json()['orders']
         dataorders.extend(tmp)
     all_data = pandas.DataFrame(dataorders)
