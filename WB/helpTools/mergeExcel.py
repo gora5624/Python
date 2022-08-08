445bb5bc48ee4bc96327dc5478ddba2fd36e4dbc
Georgyi <gora5624@gmail.com>
2022-08-08 18:52:47 +0500

    add feedack helper? bugFix

--- WB/helpTools/mergeExcel.py
+++ WB/helpTools/mergeExcel.py
@@ -1,7 +1,7 @@
 import os
 import pandas
 
-mainPath = r'C:\Users\Георгий\Downloads\report_2022_7_25аб'
+mainPath = r'C:\Users\Георгий\Downloads\report_2022_8_8'
 df = pandas.DataFrame()
 for file in os.listdir(mainPath):
     tmp = pandas.DataFrame(pandas.read_excel(os.path.join(mainPath, file)))
