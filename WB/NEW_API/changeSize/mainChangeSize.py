3d7ba6914cf1bbbd6168d43357a46a6046cc48a0
Georgyi <gora5624@gmail.com>
2022-11-11 18:10:02 +0500

    12

--- WB/NEW_API/changeSize/mainChangeSize.py
+++ WB/NEW_API/changeSize/mainChangeSize.py
@@ -5,7 +5,7 @@ from classChangeSize import filterNomenclatures1CForChange, getNomenclaturesFrom
 class changerSize():
     def __init__(self) -> None:
         self.listNomenclatures = []
-        self.tokenList = ['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjEyODkyYmRkLTEwMTgtNDJhNi1hYzExLTExODExYjVhYjg4MiJ9.nJ82nhs9BY4YehzZcO5ynxB0QKI-XmHj16MBQlc2X3w',
+        self.tokenList = [
                     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjQ3YjBiYmJkLWQ2NWMtNDNhMi04NDZjLWU1ZDliMDVjZDE4NiJ9.jcFv0PeJTKMzovcugC5i0lmu3vKBYMqoKHi_1jPGqjM',
                     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjM3ZGIyZjExLTYyMmYtNDhkNC05YmVhLTE3NWUxNDRlZWVlNSJ9.yMAeIv0WWmF3rot06aPraiQYDOy522s5IYnuZILfN6Y']
 
@@ -17,19 +17,19 @@ class changerSize():
         # self.listNomenclatures = filterNom.getNom()
         listNomenclatures= filterNom.getNom()
         for token in self.tokenList:
-            # pool = multiprocessing.Pool(30)
+            pool = multiprocessing.Pool()
             # self.listNomenclatures = listNomenclatures
             listProcess = []
-            for i in  range(0,len(listNomenclatures),25):
-                for j in listNomenclatures[i:i+25]:
-                    p = multiprocessing.Process(target=self.changeSize, args=(j['Штрихкод'], token,))
-                    p.start()
-                    listProcess.append(p)
-                for p in listProcess:
-                    p.join()
-            #     pool.apply_async(self.changeSize, args=(nomenclature['Штрихкод'], token,))
-            # pool.close()
-            # pool.join()
+            for i in  listNomenclatures:# range(0,len(listNomenclatures),25):
+                # for j in listNomenclatures[i:i+25]:
+                #     p = multiprocessing.Process(target=self.changeSize, args=(j['Штрихкод'], token,))
+                #     p.start()
+                #     listProcess.append(p)
+                # for p in listProcess:
+                #     p.join()
+                    pool.apply_async(self.changeSize, args=(i['Штрихкод'], token,))
+            pool.close()
+            pool.join()
         
 
     def changeSize(self, barcod, token):
