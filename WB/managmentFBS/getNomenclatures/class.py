6db0455b3a073d189eb1df93e0d1655ef1b37b58
Georgiy <gora5624@gmail.com>
2022-12-08 12:36:48 +0500

    123

--- WB/managmentFBS/getNomenclatures/class.py
+++ WB/managmentFBS/getNomenclatures/class.py
@@ -3,15 +3,18 @@ import requests
 import multiprocessing
 import os
 import time
+import copy
 
 class nomenclaturesGetter():
     def __init__(self, token) -> None:
-        self.mainPath = r'\\192.168.0.33\shared\_Общие документы_\Егор\ШК'
+        self.mainPath = r'\\192.168.0.33\shared\_Общие документы_\Егор\ШК\db'
         self.urlGetNom = 'https://suppliers-api.wildberries.ru/content/v1/cards/cursor/list'
         self.timeout = 10
         self.ip = token['IPName']
         self.token = token['token']
         self.dataBase = []
+        self.nomenclatures1CDataPath = r'\\192.168.0.33\shared\_Общие документы_\Егор\ШК\ШК.txt'
+        self.nomenclatures1CData = pandas.DataFrame(pandas.read_table(self.nomenclatures1CDataPath, sep='\t'))
         
 
     def log(self, text):
@@ -25,7 +28,7 @@ class nomenclaturesGetter():
         if timeout> 300:
             return 0
         if updatedAt == '' and nmID == '':
-            jsonRequestsGetCardFirst = {
+            jsonRequestsGetNom = {
                     "sort": {
                         "cursor": {
                             'limit': 1000
@@ -39,7 +42,7 @@ class nomenclaturesGetter():
                     }
                     }
         else:
-            jsonRequestsGetCardFirst = {
+            jsonRequestsGetNom = {
                     "sort": {
                         "cursor": {
                         "updatedAt": updatedAt,
@@ -59,13 +62,17 @@ class nomenclaturesGetter():
             if timeout> 300:
                 return 0
             try:
-                responce = requests.post(self.urlGetNom, json=jsonRequestsGetCardFirst, headers=headersGetCard, timeout=timeout)
+                responce = requests.post(self.urlGetNom, json=jsonRequestsGetNom, headers=headersGetCard, timeout=timeout)
                 if responce.status_code != 200:
                     timeout += 5
                     self.log(responce.text)
                     continue
                 else:
-                    data = responce.json()['data']['cards']
+                    try:
+                        data = responce.json()['data']['cards']
+                    except requests.exceptions.JSONDecodeError:
+                        timeout += 5
+                        continue
                     if len(data) != 0:
                         for card in data:
                             dataCard = {
@@ -73,11 +80,25 @@ class nomenclaturesGetter():
                                         'object': card['object'],
                                         'brand': card['brand'],
                                         'vendorCode':card['vendorCode'],
-                                        'barcodes': ','.join(card['sizes'][0]['skus']),
+                                        # 'barcodes': ','.join(card['sizes'][0]['skus']),
                                         'ip': self.ip,
                                         'mediaFiles': card['mediaFiles']
                                     }
-                            dataCards.append(dataCard)
+                            for size in card['sizes']:
+                                dataCard.update(size)
+                                if len(size['skus'])!=0:
+                                # if len(size['skus'])>1:
+                                    # if len(size['skus']) >1:
+                                    #     print('i')
+                                    for barcode in size['skus']:
+                                        # a = {'sku':barcode}
+                                        # dataCard.update(a)
+                                        dataCard.update({'sku':barcode})
+                                        dataCardCopy = copy.copy(dataCard)
+                                        dataCards.append(dataCardCopy)
+                                else:
+                                    dataCards.append(dataCard)
+                            # dataCards.append(dataCard)
                         updatedAt = responce.json()['data']['cursor']['updatedAt']
                         nmID = responce.json()['data']['cursor']['nmID']
                         total = responce.json()['data']['cursor']['total']
@@ -94,7 +115,7 @@ class nomenclaturesGetter():
          
 
     def getPiaceOfNom(self):
-        try:
+        # try:
             updatedAt = nmID = ''
             while True:
                 try:
@@ -115,63 +136,134 @@ class nomenclaturesGetter():
                     break
                 else:
                     continue
-        except:
-            self.createDB()
+        # except:
+        #     self.createDB()
 
 
     def createDB(self):
+        df = pandas.DataFrame(self.dataBase)
+        df['barcode'] = df['sku'].astype('string')
+        self.nomenclatures1CData['Штрихкод'] = self.nomenclatures1CData['Штрихкод'].astype('string')
+        df = pandas.merge(df, self.nomenclatures1CData, how='left', left_on='sku', right_on='Штрихкод')
+        df = df.drop(columns=['Штрихкод','barcode'])
         if len(list(self.dataBase)) < 1000000:
-            df = pandas.DataFrame(self.dataBase)
-            df.to_excel(os.path.join(self.mainPath,'cardsTMP {}.xlsx'.format(self.ip)), index=False)
-            df.to_csv(os.path.join(self.mainPath,'cardsTMP {}.txt'.format(self.ip)), sep='\t')
-        else:
-            df = pandas.DataFrame(self.dataBase)
-            df.to_csv(os.path.join(self.mainPath,'cardsTMP {}.txt'.format(self.ip)), sep='\t')
+            df.to_excel(os.path.join(self.mainPath,'db_nom {}.xlsx'.format(self.ip)), index=False)
+        df.to_csv(os.path.join(self.mainPath,'db_nom {}.txt'.format(self.ip)), sep='\t', index=False)
+        # else:
+        #     df = pandas.DataFrame(self.dataBase)
+        #     df['barcode'] = df['sku'].astype('string')
+        #     self.nomenclatures1CData['Штрихкод'] = self.nomenclatures1CData['Штрихкод'].astype('string')
+        #     df = pandas.merge(df, self.nomenclatures1CData, how='left', left_on='sku', right_on='Штрихкод')
+        #     df = df.drop(columns='Штрихкод')
+        #     df.to_csv(os.path.join(self.mainPath,'db_nom {}.txt'.format(self.ip)), sep='\t', index=False)
+
+class cardGetter():
+    def __init__(self, token) -> None:
+        self.mainPath = r'\\192.168.0.33\shared\_Общие документы_\Егор\ШК\db'
+        self.urlGetCard = 'https://suppliers-api.wildberries.ru/content/v1/cards/filter'
+        self.listDbNom = []
+        self.listVendorCodeToGet = []
+        self.listVendorCodeDone = []
+        self.DbCards = []
+        self.pathToDbNom = token['pathToDB']
+        self.ip = token['IPName']
+        self.token = token['token']
+        self.nomenclatures1CDataPath = r'\\192.168.0.33\shared\_Общие документы_\Егор\ШК\ШК.txt'
+        self.nomenclatures1CData = pandas.DataFrame(pandas.read_table(self.nomenclatures1CDataPath, sep='\t'))
+        self.getListVendorCode()
+    
+    
+    def getListVendorCode(self):
+        self.listVendorCodeToGet = pandas.DataFrame(pandas.read_table(self.pathToDbNom, sep='\t'))['vendorCode'].values.tolist()
 
 
-    # def start(self):
-    #     tokens = [
-    #         {
-    #             'IPName': 'Самвел',
-    #             'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjM3ZGIyZjExLTYyMmYtNDhkNC05YmVhLTE3NWUxNDRlZWVlNSJ9.yMAeIv0WWmF3rot06aPraiQYDOy522s5IYnuZILfN6Y'
-    #         },
-    #         {
-    #             'IPName': 'Караханян',
-    #             'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjEyODkyYmRkLTEwMTgtNDJhNi1hYzExLTExODExYjVhYjg4MiJ9.nJ82nhs9BY4YehzZcO5ynxB0QKI-XmHj16MBQlc2X3w'
-    #         },
-    #         {
-    #             'IPName': 'Манвел',
-    #             'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjQ3YjBiYmJkLWQ2NWMtNDNhMi04NDZjLWU1ZDliMDVjZDE4NiJ9.jcFv0PeJTKMzovcugC5i0lmu3vKBYMqoKHi_1jPGqjM'
-    #         }             
-    #     ]
-    #     pool = multiprocessing.Pool(3)
-    #     for token in tokens:
-    #         getter = nomenclaturesGetter(token)
-    #         pool.apply_async(getter.getPiaceOfNom)
-    #     pool.close()
-    #     pool.join()
+    def getNom(self):
+        # pool = multiprocessing.Pool(1)
+        # for i in range(0,len(self.listVendorCodeToGet),100):
+        #     pool.apply_async(self.getNomProcess, args=(self.listVendorCodeToGet[i:i+100], ))
+        # pool.close()
+        # pool.join()
+        for i, vendorCode in enumerate(self.listVendorCodeToGet):
+            if vendorCode not in self.listVendorCodeDone:
+                self.getNomProcess([vendorCode], i)
+        df = pandas.DataFrame(self.DbCards)
+        df = pandas.merge(df, self.nomenclatures1CData, how='left', left_on='sku', right_on='Штрихкод')
+        df = df.drop(columns='Штрихкод')
+        if len(self.DbCards) <1000000:
+            df.to_excel(os.path.join(self.mainPath, 'DB_card {}.xlsx'.format(self.ip)), index=False)
+        df.to_csv(os.path.join(self.mainPath, 'DB_card {}.txt'.format(self.ip)), index=False, sep='\t')
+
+
+    def getNomProcess(self, vendorCodes, i, timeout=20):
+        headersGetCard = {'Authorization': '{}'.format(self.token)}
+        jsonRequestsGetCard = {
+                            "vendorCodes": vendorCodes
+                            }
+        while True:
+            try:
+                responce = requests.post(url=self.urlGetCard, json=jsonRequestsGetCard, headers=headersGetCard, timeout=timeout)
+                if responce.status_code != 200:
+                    timeout+=5
+                    continue
+                try:
+                    data = responce.json()['data']
+                except requests.exceptions.JSONDecodeError:
+                    timeout+=5
+                    continue
+                for card in data:
+                    self.listVendorCodeDone.append(card['vendorCode'])
+                    try:
+                        self.listVendorCodeToGet.remove(card['vendorCode'])
+                    except ValueError:
+                        pass
+                    dataNew = {
+                        'imtID':card['imtID'],
+                        'nmID':card['nmID'],
+                        'vendorCode':card['vendorCode'],
+                        'mediaFiles':card['mediaFiles'],
+                        'group': i
+                        # 'sku':'',
+                        # 'skus':'',
+                        # 'isManySkus':'',
+                        # 'price':'',
+                        # 'chrtID':''
+                    }
+                    for char in card['characteristics']:
+                        dataNew.update(char)
+                    for size in card['sizes']:
+                        dataNew.update(size)
+                        if len(size['skus']) > 1:
+                            dataNew.update({'isManyskus':True})
+                        else:
+                            dataNew.update({'isManyskus':False})
+                        if len(size['skus']) != 0:
+                            for sku in size['skus']:
+                                dataNew.update({'sku':sku})
+                                self.DbCards.append(dataNew)
+                        else:
+                            self.DbCards.append(dataNew)          
+                self.DbCards                                         
+                break
+                # self.DbCards
+                # df = pandas.DataFrame(self.DbCards)
+                # df.to_excel(r'E:\\tmp.xlsx', index=False)
+                    
+            except requests.exceptions.ReadTimeout:
+                timeout+=5
+                continue
+            except requests.exceptions.ConnectionError:
+                timeout+=5
+                continue
+        
 
 if __name__ == "__main__":
     start_time = time.time()
-    tokens = [
-                {
-                    'IPName': 'Самвел',
-                    'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjM3ZGIyZjExLTYyMmYtNDhkNC05YmVhLTE3NWUxNDRlZWVlNSJ9.yMAeIv0WWmF3rot06aPraiQYDOy522s5IYnuZILfN6Y'
-                },
-                {
-                    'IPName': 'Караханян',
-                    'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjEyODkyYmRkLTEwMTgtNDJhNi1hYzExLTExODExYjVhYjg4MiJ9.nJ82nhs9BY4YehzZcO5ynxB0QKI-XmHj16MBQlc2X3w'
-                },
-                {
-                    'IPName': 'Манвел',
-                    'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjQ3YjBiYmJkLWQ2NWMtNDNhMi04NDZjLWU1ZDliMDVjZDE4NiJ9.jcFv0PeJTKMzovcugC5i0lmu3vKBYMqoKHi_1jPGqjM'
-                }             
-            ]
-    pool = multiprocessing.Pool(3)
-    for token in tokens:
-        getter = nomenclaturesGetter(token)
-        pool.apply_async(getter.getPiaceOfNom)
-    pool.close()
-    pool.join()
+    b = {
+                        'IPName': 'Самвел',
+                        'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjM3ZGIyZjExLTYyMmYtNDhkNC05YmVhLTE3NWUxNDRlZWVlNSJ9.yMAeIv0WWmF3rot06aPraiQYDOy522s5IYnuZILfN6Y'
+                    }
+    a = cardGetter(r'\\192.168.0.33\shared\_Общие документы_\Егор\ШК\db\db_nom Самвел.txt', b)
+    a.getListVendorCode()
+    a.getNom()
 
     print("--- %s seconds ---" % (time.time() - start_time))
